22156afeedb3240905d2215648ba2dcb3e4a409e
Shulammite Ughojo <emetila83@gmail.com>
2025-06-02 07:37:13 +0100

    Setup Expo project
    - installed expo project
    - setup colors constant

---
+++ logirate/app/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.background,
+        flex: 1,
+        paddingHorizontal: '10%'
+    },
+})
+
+export default styles;
---
+++ logirate/constants/Colors.ts
@@ -0,0 +1,25 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+// const tintColorLight = '#0a7ea4';
+// const tintColorDark = '#fff';
+
+const Colors = {
+  primary: '#00A1BF',
+  secondary: '#0F141A',
+  info: '#2F80ED',
+  success: '#27AE60',
+  warning: '#E2B93B',
+  error: '#EB5757',
+  white: '#FFFFFF',
+  background: '#0F141A',
+  text: '#0F141A',
+  buttonBg: '#00A1BF',
+  buttonText: '#FFF',
+  inputBg: "#232533",
+  inputBgTwo: '#1E1E2D',
+};
+
+export default Colors;
